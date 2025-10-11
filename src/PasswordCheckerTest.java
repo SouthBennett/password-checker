94ea77cefb0e8b70bb035524358b6ce51503c551
samkosal <67755908+samkosal@users.noreply.github.com>
2025-10-11 12:54:54 -0800

    finished the first two public constructor

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,8 +4,39 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
 
+  // test for the first public method describePasswordLength: checks whether the password length is within short and medium threshold
+
+  // Test 1: check the length of "hello"
   @Test
   public void testDescribePasswordLength() {
+    //arrange
     PasswordChecker word = new PasswordChecker(5, 6);
-    assertEquals(2, 0);
+    //asert
+    String actual = word.describePasswordLength("hello");
+    //act
+    assertEquals("medium", actual);
+  }
+  // Test 2:
+
+  // Test 3:
+
+  // Test for the second public method isAlphanumeric: checks whether the string has only numbers and string
+
+  // Test 1: basic check, checks "Hello1" 
+  @Test
+  public void testisAlphanumeric() {
+    //arrange
+    PasswordChecker word = new PasswordChecker(5, 6);
+    //assert
+    boolean actual = word.isAlphanumeric("Hello1");
+    //act
+    assertEquals(true, actual);
+
+  }
+
+  // Test 2: maybe, check if it only has numbers or string.
+
+  // Test 3: maybe check if its empty, or has special characters or stuff.
+
+  public void 
 }
