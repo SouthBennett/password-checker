838e7361cef2b42df79d54f69832116b0f43005b
samkosal <67755908+samkosal@users.noreply.github.com>
2025-10-11 17:09:22 -0800

    Added more instruction to all constructor test, Trying to finish the last contructor, did not work

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
@@ -16,9 +20,27 @@ public class PasswordCheckerTest {
     //act
     assertEquals("medium", actual);
   }
-  // Test 2:
+  // Test 2: testing a long string
+  @Test
+  public void testDescribePasswordLengthLong() {
+    //arrange
+    PasswordChecker word = new PasswordChecker(5, 6);
+    //asert
+    String actual = word.describePasswordLength("hello123123123123123123123123123123123123123123");
+    //act
+    assertEquals("long", actual);
+  }
 
-  // Test 3:
+  // Test 3: testing an empty string
+  @Test
+  public void testDescribePasswordLengthShort() {
+    //arrange
+    PasswordChecker word = new PasswordChecker(1, 2);
+    //asert
+    String actual = word.describePasswordLength("");
+    //act
+    assertEquals("short", actual);
+  }
 
   // Test for the second public method isAlphanumeric: checks whether the string has only numbers and string
 
@@ -38,5 +60,20 @@ public class PasswordCheckerTest {
 
   // Test 3: maybe check if its empty, or has special characters or stuff.
 
-  public void 
+  //Test for the third public method isBannedPassword: check whether the password is banned, returns true if the password exist in the banned password set
+  // Test 1: basic check, checks whether "password123" exist. 
+  @Test
+  public void testisBannedPassword() {
+
+    // this part right here, the string lebron is not being added, i dont understand why.?????
+    Set<String> banned = new HashSet<>(Arrays.asList("Lebron"));
+    PasswordChecker word = new PasswordChecker(5, 6, banned);
+
+    boolean actual = word.isBannedPassword("Lebron");
+
+    assertEquals(true, actual);
+  }
+  // Test 2: maybe add a list of random strings and check if returning one of them is true
+
+  // Test 3: maybe check if its empty, or has special characters or stuff.
 }
